5baf23cc7095c66fbfe0852f6f3c7d40242cc5e3
BillC424 <wcurran44@gmail.com>
2022-07-01 14:58:41 -0400

    Added nested method to iterate through the entire alphabet for each letter in the string to determine which index number is associated with the alphabet letter, subtract it by the shift number, and return the string shifted

--- caesar.rb
+++ caesar.rb
@@ -1,7 +1,15 @@
-alphabet_reversed = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"]
-
-def caesar_cipher(string)
-p string.split(//)
-end
-
-caesar_cipher("yooooooo")
+def caesar_cipher(string, shift)
+    alphabet_reversed =["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"]
+  split_string = string.split(//)
+  indexes = []
+  split_string.select do |c_letter| 
+     alphabet_reversed.each_with_index do |letter, index| 
+       if letter == c_letter 
+         indexes = indexes.push(alphabet_reversed[index - shift])
+       end
+     end
+   end 
+  p indexes
+  end
+  
+  p caesar_cipher("zavpy", 4) 
