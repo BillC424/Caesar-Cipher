09c3ea1f9704ec5980601f5a6370ce2f577800a2
BillC424 <wcurran44@gmail.com>
2022-07-01 12:58:07 -0400

    Added an array to reference every letter of the alphabet and created a function that will split up a string input into individual letters

---
+++ caesar.rb
@@ -0,0 +1,7 @@
+alphabet_reversed = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"]
+
+def caesar_cipher(string)
+p string.split(//)
+end
+
+caesar_cipher("yooooooo")
