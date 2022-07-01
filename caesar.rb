e1e3be66c91c13b0321a5062428eb174ce5cf99c
BillC424 <wcurran44@gmail.com>
2022-07-01 17:00:13 -0400

    Updated method to account for spaces and capitalization

--- caesar.rb
+++ caesar.rb
@@ -1,15 +1,29 @@
 def caesar_cipher(string, shift)
+  
     alphabet_reversed =["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"]
-  split_string = string.split(//)
+    capital_reversed = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+  
+  string_letters = string.chars
   indexes = []
-  split_string.select do |c_letter| 
+  string_letters.select do |c| 
+    if c == " " || c == "!" || c == "," || c == "?" || c == "." || c == ";" || c == ":"
+      indexes = indexes.push(c)
+    end  
+    if c == c.upcase 
+       capital_reversed.each_with_index do |letter, index| 
+       if letter == c
+         indexes = indexes.push(capital_reversed[index - shift])
+           end
+         end
+    end
      alphabet_reversed.each_with_index do |letter, index| 
-       if letter == c_letter 
+       if letter == c 
          indexes = indexes.push(alphabet_reversed[index - shift])
        end
      end
    end 
-  p indexes
+  
+  indexes.join
   end
   
-  p caesar_cipher("zavpy", 4) 
+  p caesar_cipher("What a String!", 4) 
